94047729eed65276139e1ca552850faed17a3a42
Kai-TRD <wkailuo03@gmail.com>
2022-11-15 09:07:33 +0100

    change

---
+++ xmassTree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class xmassTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime la altura");
+        sc.close();
+
+
+
+
+
+    }
+}
